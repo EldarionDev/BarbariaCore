0fec9f2d9a7a963801bb29973ee082d72d4143f8
EldarionDev <janek.schultes.privat@gmail.com>
2020-12-19 17:40:32 +0100

    Initial commit

---
+++ src/main/java/barbaria/barbaria/client/core/ModKeys.java
@@ -0,0 +1,36 @@
+package barbaria.barbaria.client.core;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.client.util.InputMappings;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import org.lwjgl.glfw.GLFW;
+
+
+@OnlyIn(Dist.CLIENT)
+public class ModKeys {
+    private static final String CATEGORY = "Barbaria Controls";
+    private static final String OPEN_FACTION_MENU = "Open the faction menu";
+
+    private static final KeyBinding FACTION_MENU_KEY = new KeyBinding(OPEN_FACTION_MENU, KeyConflictContext.IN_GAME,
+            InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_V, CATEGORY);
+
+    public static void register() {
+        MinecraftForge.EVENT_BUS.register(new ModKeys());
+        ClientRegistry.registerKeyBinding(FACTION_MENU_KEY);
+    }
+
+    @SubscribeEvent
+    public void onKeyPress(InputEvent event) {
+        if (FACTION_MENU_KEY.isKeyDown()) {
+            Minecraft.getInstance().world.setDayTime(0);
+        }
+    }
+}
